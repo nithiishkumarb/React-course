51083bb047dfdd052b04ee0791d2d8ba78fd8b35
nithiishkumarb <nithiishkumarb@gmail.com>
2025-04-20 10:57:57 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,12 @@
+
+const parent=React.createElement(
+    "div",{id:"parent"},
+    [ React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"Hello this is  h1 tag"),
+        React.createElement("h2",{},"Hello this is  h2 tag")]), React.createElement("div",{id:"child"},
+            [React.createElement("h1",{},"Hello this is  h1 tag"),
+            React.createElement("h2",{},"Hello this is  h2 tag")])
+    ]
+)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
